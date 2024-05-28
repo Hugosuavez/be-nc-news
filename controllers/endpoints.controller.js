6ab2edbae5b1157cbae3ba6edaca98c7c3abd139
Hugosuavez <153728484+Hugosuavez@users.noreply.github.com>
2024-05-28 13:46:26 +0100

    add tests and endpoint for get /api

---
+++ controllers/endpoints.controller.js
@@ -0,0 +1,9 @@
+const fs = require('fs/promises')
+
+exports.getEndpoints = (req, res, next) => {
+    fs.readFile('./endpoints.json', 'utf-8')
+    .then((endpoints) => {
+        res.status(200).send({endpoints})
+    })
+    .catch(next)
+}

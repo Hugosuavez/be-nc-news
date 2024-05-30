66de7798258b53e3e5772fd93836fad1520ef86d
Hugosuavez <153728484+Hugosuavez@users.noreply.github.com>
2024-05-30 13:41:41 +0100

    add tests and solution for get user by username

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -1,8 +1,21 @@
-const {fetchUsers} = require('../models/users.models')
+const {fetchUsers, fetchUserByUsername, checkUserExists} = require('../models/users.models')
 
 exports.getUsers = (req, res, next) => {
     fetchUsers().then((users) => {
         res.status(200).send({users})
+    })
+    .catch(next)
+}
+
+exports.getUserByUsername = (req, res, next) => {
+    const { username } = req.params
+
+    const promises = [checkUserExists(username), fetchUserByUsername(username)]
+
+    Promise.all(promises)
+    .then((resolvedPromises) => {
+        const user = resolvedPromises[1]
+        res.status(200).send({user})
     })
     .catch(next)
 }

723e06ea78dab9b61c97131d288c6328e0ed2974
Hugosuavez <153728484+Hugosuavez@users.noreply.github.com>
2024-05-30 12:31:44 +0100

    refactor using express routers

--- app.js
+++ app.js
@@ -1,32 +1,10 @@
 const express = require('express')
 const app = express()
-const { getTopics } = require('./controllers/topics.controllers')
-const { getEndpoints } = require('./controllers/endpoints.controller')
-const { getArticleById, getArticles, updateVotes } = require('./controllers/articles.controllers')
-const { getCommentsByArticleId, postCommentByArticleId, deleteComment, getCommentByCommentId } = require('./controllers/comments.controllers')
-const { getUsers } = require('./controllers/users.controllers')
+const apiRouter = require('./routes/api-router')
 
 app.use(express.json())
 
-app.get('/api/topics', getTopics)
-
-app.get('/api', getEndpoints)
-
-app.get('/api/articles', getArticles)
-
-app.get('/api/articles/:article_id', getArticleById)
-
-app.get('/api/articles/:article_id/comments', getCommentsByArticleId)
-
-app.post('/api/articles/:article_id/comments', postCommentByArticleId)
-
-app.patch('/api/articles/:article_id', updateVotes)
-
-app.delete('/api/comments/:comment_id', deleteComment)
-
-app.get('/api/comments/:comment_id', getCommentByCommentId)
-
-app.get('/api/users', getUsers)
+app.use('/api', apiRouter)
 
 app.use((err, req, res, next) => {
     if(err.msg){
